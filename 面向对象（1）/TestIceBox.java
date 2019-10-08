48dcff69b266a7bebb62dac3f9e3524a5b37a022
Minikevin <56224257+Minikevin@users.noreply.github.com>
2019-10-08 16:51:15 +0800

    Add files via upload

---
+++ 面向对象（1）/TestIceBox.java
@@ -0,0 +1,13 @@
+package cn.gdgm.projectdemo;
+
+public class TestIceBox {
+
+	public static void main(String[] args) {
+		// 把大象放进冰里
+		IceBox.putThings("大象");
+		//把可乐放到冰箱里
+		IceBox.putThings("可乐");
+
+	}
+
+}
